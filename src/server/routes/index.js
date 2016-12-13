f5301250fa5008d2f4287d0a0da57974a04e9c2b
dmstyer <dmstyer@gmail.com>
2016-12-13 13:19:41 -0700

    added materialize

--- src/server/routes/index.js
+++ src/server/routes/index.js
@@ -4,9 +4,13 @@ const router = express.Router();
 const indexController = require('../controllers/index');
 
 router.get('/', function (req, res, next) {
-  const renderObject = {};
+  var renderObject = {};
   res.render('index', renderObject);
 });
 
+router.get('/team', function (req, res, next) {
+  var renderObject = {};
+  res.render('team', renderObject);
+});
 
 module.exports = router;
