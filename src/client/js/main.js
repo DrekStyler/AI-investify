8476c09c9ec0b6b020adf76a0b31e2e6f1292fe4
dmstyer <dmstyer@gmail.com>
2016-12-13 15:20:52 -0700

    added questions

--- src/client/js/main.js
+++ src/client/js/main.js
@@ -1,6 +1,20 @@
 (function () {
   console.log('sanity check!');
+  var q_counter = 0;
+  var questions = [
+    'How long has the current core of the company worked together?',
+    'What is the fastest animal in the world?',
+    'What is my favorite color?'
+  ];
 
-var questions = ['How long has the current core of the company worked together?', 'What is the fastest animal in the world?'];
-
+$('#change_question').on('click', ()=> {
+  $('#change_question').text('Next Question');
+  $('#question_container').html(`<b>${questions[q_counter]}</b>`);
+  if (q_counter >= questions.length) {
+    $('#question_container').html(`<b>Please Proceed to the Next Section</b>`);
+    $('#change_question').text('Next Section');
+  } else {
+      q_counter++;
+  }
+});
 })();
