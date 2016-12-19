dbe00f0c2543e44cb9952896efc9b4890961d081
dmstyer <dmstyer@gmail.com>
2016-12-19 03:24:00 -0700

    working multiple graphs

--- src/client/js/market.js
+++ src/client/js/market.js
@@ -1,14 +1,70 @@
 console.log('market.js');
+let groupFifteen = 6361;
+let groupTwentyFive = 20047;
+let groupThirtyFive = 21222;
+let groupFortyFive = 23294;
+let groupFiftyFive = 23896;
+let groupSixtyFivePlus = 30998;
+let totalHouseholds = 125818;
+let selectedGroup = 0;
+let remainingHouseholds = 0;
+let selectedLabel;
 
 $(document).ready(function() {
     $('select').material_select();
+    $('#target_age').change(function () {
+      switch ($('#target_age').val()) {
+        case '15-24':
+        selectedGroup = groupFifteen;
+        remainingHouseholds = totalHouseholds - groupFifteen;
+        selectedLabel = '15-24';
+            break;
+          case '25-34':
+          selectedGroup = groupTwentyFive;
+          remainingHouseholds = totalHouseholds - groupTwentyFive;
+          selectedLabel = '25-34';
+            break;
+          case '35-44':
+          selectedGroup = groupThirtyFive;
+          remainingHouseholds = totalHouseholds - groupThirtyFive;
+          selectedLabel = '35-44';
+            break;
+          case '45-54':
+            selectedGroup = groupFortyFive;
+            remainingHouseholds = totalHouseholds - groupFortyFive;
+            selectedLabel = '45-54';
+              break;
+          case '55-44':
+            selectedGroup = groupFiftyFive;
+            remainingHouseholds = totalHouseholds - groupFiftyFive;
+            selectedLabel = '55-64';
+              break;
+          case '65+':
+            selectedGroup = groupSixtyFivePlus;
+            remainingHouseholds = totalHouseholds - groupSixtyFivePlus;
+            selectedLabel = '65+';
+              break;
+        default:
+          console.log('nope');
+      }
+      chart.data.labels = [selectedLabel,"Remaining Population"];
+      chart.data.datasets = [{
+          backgroundColor: [
+            "#2ecc71",
+            "#3498db"
+          ],
+          data: [selectedGroup, remainingHouseholds]
+        }];
+      chart.update();
+    });
   });
 
+
   var ctx = document.getElementById("chart-area").getContext('2d');
-  var myChart = new Chart(ctx, {
+  var chart = new Chart(ctx, {
     type: 'pie',
     data: {
-      //labels: ["M", "T", "W", "T", "F", "S", "S"],
+      labels: ["15-24", "25-34", "35-44", "45-54", "55-64", "65+"],
       datasets: [{
         backgroundColor: [
           "#2ecc71",
@@ -16,10 +72,9 @@ $(document).ready(function() {
           "#95a5a6",
           "#9b59b6",
           "#f1c40f",
-          "#e74c3c",
-          "#34495e"
+          "#e74c3c"
         ],
-        data: [120, 19, 3, 17, 28, 24, 7]
+        data: [groupFifteen, groupTwentyFive, groupThirtyFive, groupFortyFive, groupFiftyFive, groupSixtyFivePlus]
       }]
     }
   });
